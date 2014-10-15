5425a580693b8358c2287e94bf6bfb25f5b1dedb
ymlbright <yml_bright@163.com>
2014-10-15 21:04:37 +0800

    modhandler threading

--- mod/ModHandler.py
+++ mod/ModHandler.py
@@ -3,27 +3,28 @@
 # @Date    : 2014-10-12 13:06:31
 # @Author  : yml_bright@163.com
 
-import os
-
+import os, threading
 
 class ReportObject(object):
-	def __init__(self, Type, DomainName, SIP, DIP, Timestamp, Optional):
-		self.Type = Type
-		self.DomainName = DomainName
-		self.SIP = SIP
-		self.DIP = DIP
-		self.Timestamp = Timestamp
-		self.Optional = Optional
+    def __init__(self, Type, DomainName, SIP, DIP, Timestamp, Optional):
+        self.Type = Type
+        self.DomainName = DomainName
+        self.SIP = SIP
+        self.DIP = DIP
+        self.Timestamp = Timestamp
+        self.Optional = Optional
 
-class ModHandler(object):
-	def __init__(self, metaQueue, reportQueue):
-		self.metaQueue = metaQueue
-		self.reportQueue = reportQueue
+class ModHandler(threading.Thread):
+    def __init__(self, metaQueue, reportQueue):
+        threading.Thread.__init__(self)
+        self.daemon = False
+        self.metaQueue = metaQueue
+        self.reportQueue = reportQueue
 
-	def get(self):
-		if self.metaQueue.qsize() <=0:
-			return None
-		return self.metaQueue.get()
+    def get(self):
+        if self.metaQueue.qsize() <=0:
+            return None
+        return self.metaQueue.get()
 
-	def report(self, Type, DomainName, SIP, DIP, Timestamp, Optional=None):
-		self.reportQueue.put(ReportObject(Type, DomainName, SIP, DIP, Timestamp, Optional))
+    def report(self, Type, DomainName, SIP, DIP, Timestamp, Optional=None):
+        self.reportQueue.put(ReportObject(Type, DomainName, SIP, DIP, Timestamp, Optional))
